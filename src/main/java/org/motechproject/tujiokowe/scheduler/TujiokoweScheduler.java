b31ef6461fda226dad5c0091aad09324103eaa44
Pawel Muchowski <pmuchowski@soldevelo.com>
2020-04-18 01:48:54 +0200

    UMURINZI-34: Show progress on the export modal

--- src/main/java/org/motechproject/tujiokowe/scheduler/TujiokoweScheduler.java
+++ src/main/java/org/motechproject/tujiokowe/scheduler/TujiokoweScheduler.java
@@ -3,7 +3,10 @@ package org.motechproject.tujiokowe.scheduler;
 import java.util.HashMap;
 import java.util.Map;
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.joda.time.Period;
+import org.motechproject.commons.date.model.Time;
+import org.motechproject.commons.date.util.DateUtil;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.scheduler.contract.RepeatingPeriodSchedulableJob;
 import org.motechproject.scheduler.service.MotechSchedulerService;
@@ -49,5 +52,16 @@ public class TujiokoweScheduler {
 
   public void unscheduleFetchCsvJob() {
     motechSchedulerService.safeUnscheduleAllJobs(TujiokoweConstants.FETCH_CSV_EVENT);
+  }
+
+  public void scheduleClearExportTasksJob() {
+    DateTime startDate =  DateUtil.newDateTime(LocalDate.now().plusDays(1),
+        Time.parseTime(TujiokoweConstants.CLEAR_EXPORT_TASKS_EVENT_START_TIME, ":"));
+    Period period = Period.days(1);
+
+    MotechEvent event = new MotechEvent(TujiokoweConstants.CLEAR_EXPORT_TASKS_EVENT);
+
+    RepeatingPeriodSchedulableJob job = new RepeatingPeriodSchedulableJob(event, startDate.toDate(), null, period, true);
+    motechSchedulerService.safeScheduleRepeatingPeriodJob(job);
   }
 }
